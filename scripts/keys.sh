e651e01fa1568555909b5376bcbc5ed7ac30d939
morogoyo <morogoyo@gmail.com>
2020-08-26 10:41:47 -0400

    refactored  scripts to handle ssh keys and added directories to ignore file

--- scripts/keys.sh
+++ scripts/keys.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-mkdir /home/ansible/.ssh
-cat /home/ansible/keys/git/git_hub_id_rsa > /home/ansible/.ssh/git.id_rsa
-cat /home/ansible/keys/linux-academy/academy.id_rsa  > /home/ansible/.ssh/vagrant.id_rsa
+
+if [ ! -d "/home/ansible/.ssh/" ]
+then
+mkdir /home/ansible/.ssh/
+cat /home/ansible/keys/git_hub_id_rsa > /home/ansible/.ssh/git.id_rsa
+cat /home/ansible/keys/academy.id_rsa  > /home/ansible/.ssh/vagrant.id_rsa
+fi
